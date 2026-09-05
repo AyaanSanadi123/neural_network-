bb761401b0275bf0c195c041a290a69b2b40deb7
Ayaan Sanadi <ayaansanadi8@gmail.com>
2026-09-05 17:45:55 +0530

    added thread_pool to nn

--- optimizer/optimizer.c
+++ optimizer/optimizer.c
@@ -1,6 +1,8 @@
 #include<optimizer.h>
 #include <stdlib.h>
 #include<matrix.h>
+
+// SGD
 void sgd_update(layer* l, double lr){
     // 1. Scale the raw gradients by the learning rate: (lr * dW)
     Matrix* scaled_dW = matrix_multiply_scalar(l->d_weights, lr);
@@ -26,5 +28,66 @@ Optimizer* create_sgd_optimizer(double learning_rate){
     Optimizer* opt = (Optimizer*)malloc(sizeof(Optimizer));
     opt->learning_rate = learning_rate;
     opt->update_func = sgd_update;
+    return opt;
+}
+
+
+// ADAM 
+
+
+void adam_update(Optimizer* opt, layer* l, int layer_index){
+    AdamState* state = (AdamState*) opt -> state; // caste it from void to AdamState
+    AdamLayerCache cache = state -> layer_caches[layer_index]; // get the cache values of the current layer 
+
+    // get the global variables and populate the local variables 
+    double lr = opt -> learning_rate;
+    double b1 = state ->beta1;
+    double b2 = state -> beta2;
+    double eps = state -> epsilon;
+    double t = (double) state -> t;
+}
+
+
+Optimizer* create_adam_optimizer(double learning_rate, int num_layers, layer** layers){
+    // create the opt object 
+    Optimizer* opt = (Optimizer*) malloc (sizeof(Optimizer));
+    opt -> learning_rate = learning_rate;
+    opt -> update_func = adam_update;
+    // declare and populate the state
+    AdamState* state = (AdamState*)malloc(sizeof(AdamState));
+    state -> t = 0;
+    state -> beta1 = 0.9;
+    state -> beta2 = 0.999;
+    state -> epsilon = 1e-8;
+
+    state -> layer_caches = (AdamLayerCache*)malloc(num_layers * sizeof(AdamLayerCache));
+
+    for (int i = 0; i < num_layers; i++)
+    {
+        int w_rows = layers[i] ->weights -> rows;
+        int w_cols = layers[i] -> weights -> cols;
+
+        int b_rows = layers[i] -> biases -> rows;
+        int b_cols = layers[i] -> biases -> cols;
+
+        state -> layer_caches[i].m_weights = create_matrix(w_rows,w_cols);
+        state -> layer_caches[i].v_weights = create_matrix(w_rows,w_cols);
+
+        state -> layer_caches[i].m_biases = create_matrix(b_rows,b_cols);
+        state -> layer_caches[i].v_biases = create_matrix(b_rows,b_cols);
+
+        // populated the cache layers with 0's
+        for(int j = 0; j< w_rows * w_cols ; j++){
+            state -> layer_caches[i].m_weights -> data[j] = 0.0;
+            state -> layer_caches[i].v_weights -> data[j] = 0.0;
+        }
+        for(int j = 0; j< b_rows * b_cols ; j++){
+            state -> layer_caches[i].m_biases -> data[j] = 0.0;
+            state -> layer_caches[i].v_biases -> data[j] = 0.0;
+        }
+    }
+    
+    opt -> state = state;
+
     return opt;
 }
--- optimizer/optimizer.h
+++ optimizer/optimizer.h
@@ -5,9 +5,32 @@
 #include "matrix.h"
 
 typedef struct {
+    Matrix* m_weights;
+    Matrix* v_weights;
+    Matrix* m_biases;
+    Matrix* v_biases;
+}  AdamLayerCache;  
+
+
+typedef struct {
+    int t;
+    double beta1;
+    double beta2;
+    double epsilon;
+    AdamLayerCache* layer_caches;
+}  AdamState;
+
+typedef struct Optimizer Optimizer;
+
+struct Optimizer{
     double learning_rate;
-    void(*update_func)(layer* l,double lr);
-}  Optimizer;
+    void* state; // if a optimizer does not need any state, state = NULL
+
+    void (*update_func)(Optimizer* opt,layer* l,int layer_index);
+};
+
 
 Optimizer* create_sgd_optimizer(double learning_rate);
+void adam_update(Optimizer* opt, layer* l, int layer_index);
+Optimizer* create_adam_optimizer(double learning_rate, int num_layers, layer** layers);
 #endif
